5f0c78951f8526e1e611b4be90a68a1cffceeaba
Ricardo Diniz <rdinizcal@gmail.com>
2017-07-02 11:35:45 -0300

    declaracao de func com tipo

--- src/main/scala/br/unb/cic/mhs/ast/DecFuncao.scala
+++ src/main/scala/br/unb/cic/mhs/ast/DecFuncao.scala
@@ -1,3 +1,3 @@
 package br.unb.cic.mhs.ast
 
-class DecFuncao (val nome: String, val args: List[String],val corpo: Expressao)
+class DecFuncao (val tipo : Tipo, val nome: String, val args: List[String],val corpo: Expressao)
--- src/test/scala/br/unb/cic/mhs/ExpressaoAplicacaoTeste.scala
+++ src/test/scala/br/unb/cic/mhs/ExpressaoAplicacaoTeste.scala
@@ -1,23 +1,25 @@
 package br.unb.cic.mhs
 
+
+
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
-import br.unb.cic.mhs.ast.DecFuncao
-import br.unb.cic.mhs.ast.ExpressaoSoma
-import br.unb.cic.mhs.ast.ValorInteiro
-import br.unb.cic.mhs.ast.Referencia
-import br.unb.cic.mhs.memoria.AmbienteDecFuncao
+
 import br.unb.cic.mhs.ast.Aplicacao
-import scala.ref.Reference
-import br.unb.cic.mhs.ast.Referencia
-import br.unb.cic.mhs.ast.Referencia
+import br.unb.cic.mhs.ast.DecFuncao
 import br.unb.cic.mhs.ast.ExpressaoLet
-import br.unb.cic.mhs.ast.Expressao
+import br.unb.cic.mhs.ast.ExpressaoSoma
+import br.unb.cic.mhs.ast.Referencia
+import br.unb.cic.mhs.ast.ValorInteiro
+import br.unb.cic.mhs.memoria.AmbienteDecFuncao
+import br.unb.cic.mhs.ast.TInteiro
+import br.unb.cic.mhs.ast.DecFuncao
+import br.unb.cic.mhs.ast.ExpressaoITE
 
 class ExpressaoAplicacaoTest extends FlatSpec with Matchers {
   
-  "uma aplicacao inc 4 (onde existe inc x = x + 1) " should " levar ao valor 5" in {
-    val inc = new DecFuncao("inc", List("x"), new ExpressaoSoma(new Referencia("x"), new ValorInteiro(1)))  
+  "uma aplicacao inc 4 (onde existe int inc x = x + 1) " should " levar ao valor 5" in {
+    val inc = new DecFuncao(TInteiro, "inc", List("x"), new ExpressaoSoma(new Referencia("x"), new ValorInteiro(1)))  
     val app = new Aplicacao("inc", new ValorInteiro(4))
     
     AmbienteDecFuncao.associar("inc", inc)
@@ -26,9 +28,9 @@ class ExpressaoAplicacaoTest extends FlatSpec with Matchers {
   }
   
   //TODO: isso eh uma aberracao, pois tem a semantica de scopo dinamico. 
-  "supondo (def f y = x + y), e avaliamos let x = 10 in f 5 " should " levar ao 15" in {
+  "supondo (int def f y = x + y), e avaliamos let x = 10 in f 5 " should " levar ao 15" in {
     val refX = new Referencia("x") 
-    val f    = new DecFuncao("f", List("y"), new ExpressaoSoma(refX, new Referencia("y")))
+    val f    = new DecFuncao(TInteiro, "f", List("y"), new ExpressaoSoma(refX, new Referencia("y")))
     val let  = new ExpressaoLet("x", new ValorInteiro(10), new Aplicacao("f", new ValorInteiro(5)))    
  
     AmbienteDecFuncao.associar("f", f)
